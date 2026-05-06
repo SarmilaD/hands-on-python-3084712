6c22375bbce4d3a42b35df334cd5ff979e0dcdd9
Sarmila dangol <dsarmila99@gmail.com>
2026-05-06 21:30:04 +0000

    Python practice code updated

--- Ex_Files/04_01_begin/main.py
+++ Ex_Files/04_01_begin/main.py
@@ -1,8 +1,16 @@
 import requests
+# Run below command in Terminal to install REQUESTS package with pip before running 'python main.py' command
+# python3 -m pip install requests
 
 response = requests.get(
     "http://api.worldbank.org/v2/countries/USA/indicators/SP.POP.TOTL?per_page=5000&format=json",
 )
 
-last_twenty_years = response.json()[1][:20]
+last_twenty_years = response.json()[1][:20] # get first 2o records from reponse json
+
+for year in last_twenty_years:
+  if not year["value"]:
+    continue
+  display_width = year["value"] // 10_000_000 # underscore keep number readable friendly
+  print(f"{year['date']}: {year['value']}", display_width * "=") # it gives date of year and its value
 
--- Ex_Files/04_02_begin/main.py
+++ Ex_Files/04_02_begin/main.py
@@ -1,3 +1,10 @@
-from flask import Flask
+from distutils.log import debug
+from flask import Flask # in Terminal run command 'pip install Flask' to install Flask for web application
 
 app = Flask(__name__)
+
+@app.route("/")
+def hello():
+  return "Hello, World!"
+
+app.run(debug=True)
